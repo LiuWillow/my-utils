c7a5ac79f57ac2ba4410afa7b461ae6d4ca0825c
LiuWillow <liuwillow9625@163.com>
2019-02-21 11:01:27 +0800

    修改图片压缩

--- src/main/com/lwl/util/MediaUtils.java
+++ src/main/com/lwl/util/MediaUtils.java
@@ -18,20 +18,40 @@ import java.util.Map;
 public class MediaUtils {
     private MediaUtils(){}
     /**
-     * 将前端传的图片压缩到为指定大小，返回压缩后的byte流和图片大小
+     * 将图片压缩到为指定大小，返回压缩后的byte流和图片大小
      */
-    public static Map<String, Object> compressImage(InputStream inputStream, int width, int height,
-                                                    String fileName){
+    public static Map<String, Object> compressImage(MultipartFile multipartFile, int width, int height) {
         try {
             //读取图片并根据参数压缩
-            Image src = ImageIO.read(inputStream);
-            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            image.getGraphics().drawImage(src, 0, 0, width, height, null);
+            Image src = ImageIO.read(multipartFile.getInputStream());
+            BufferedImage image;
+            if (width == 0 || height == 0) {
+                image = (BufferedImage) src;
+            } else {
+                int oldHeight = ((BufferedImage) src).getHeight();
+                int oldWidth = ((BufferedImage) src).getWidth();
+                if (oldHeight > oldWidth) {
+                    // 如果height比较大，就以高为准
+
+                    float rate = height / (oldHeight * 1F);
+                    width = (int) (rate * oldWidth);
+                    image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                    image.getGraphics().drawImage(src.getScaledInstance(width, height, Image.SCALE_SMOOTH),
+                            0, 0,null);
+
+                } else {
+                    // 如果width比较大，就以款宽为准
+                    float rate = width / (oldWidth * 1F);
+                    height = (int) (rate * oldHeight);
+                    image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                    image.getGraphics().drawImage(src.getScaledInstance(width, height, Image.SCALE_SMOOTH),
+                            0, 0,null);
+                }
+            }
 
             //获取图片后缀
-            String originFileName = fileName;
-            String[] originFileNameArray = originFileName.split("\\.");
-            String suffix = originFileNameArray[1];
+            String originFileName = multipartFile.getOriginalFilename();
+            String suffix = UtilTool.getMediaTypeByFileName(originFileName);
 
             //转为流
             ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -39,8 +59,10 @@ public class MediaUtils {
 
             Map<String, Object> map = new HashMap<>();
             byte[] bytes = os.toByteArray();
-            map.put("stream", new ByteArrayInputStream(bytes));
-            map.put("capacity", (long) bytes.length);
+            map.put(Constants.KEY_INPUT_STREAM, new ByteArrayInputStream(bytes));
+            map.put(Constants.KEY_CAPACITY_OF_INPUT_STREAM, (long) bytes.length);
+            map.put(Constants.KEY_HEIGHT, image.getHeight());
+            map.put(Constants.KEY_WIDTH, image.getWidth());
 
             return map;
 
